ef3be93b0bf4ca71185c11c0f22a83b3d3928050
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-19 23:22:09 +0530

    heads and tails program

---
+++ ifelse/headTail.js
@@ -0,0 +1,7 @@
+//Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+let flipcoin = Math.floor(Math.random() * 2);
+if(flipcoin==1)
+console.log("it is heads!!")
+else
+console.log("it is tails!!")
